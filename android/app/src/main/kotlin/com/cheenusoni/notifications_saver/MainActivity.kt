26f003f7e84b5e97252aff60f29fdc9dc6dc0d55
NullByte08 <43966615+NullByte08@users.noreply.github.com>
2023-10-05 12:17:25 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/cheenusoni/notifications_saver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cheenusoni.notifications_saver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatefulWidget {
+  const HomePageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
